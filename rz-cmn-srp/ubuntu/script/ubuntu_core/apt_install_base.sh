ade9f3f73dbfc35bb4dc76b5d0b54b4b8c19b335
Tien Nguyen <tien.nguyen.uh@renesas.com>
2025-06-24 23:41:15 +0700

    rz-sbc: ubuntu: manually enable and restart systemd related services
    Add commands to enable and restart systemd related services
    
    Signed-off-by: Tien Nguyen <tien.nguyen.uh@renesas.com>

--- rz-cmn-srp/ubuntu/script/ubuntu_core/apt_install_base.sh
+++ rz-cmn-srp/ubuntu/script/ubuntu_core/apt_install_base.sh
@@ -63,3 +63,13 @@ apt install -y python3-pip \
 	obexftp bluez-obexd \
 	coreutils \
 	bsdmainutils
+
+# Enabling systemd related services
+echo "Enabling and starting systemd service..."
+systemctl enable systemd-resolved.service
+systemctl enable systemd-timesyncd
+systemctl start systemd-resolved.service
+systemctl start systemd-timesyncd
+
+rm -f /etc/resolv.conf
+ln -s /run/systemd/resolve/stub-resolv.conf /etc/resolv.conf
